34d249a18d21403a38ba000b12badfbdacdec7cd
RBang2501 <rakshitbang0005@gmail.com>
2023-11-07 14:28:24 +0530

    Testing Final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,7 +18,7 @@ public class Main {
     public static double division(int a, int b) {
         if (b == 0) {
             System.out.println("Sorry ! " +
-                    "division  by Zero Not Possible");
+                    "division  by ssible");
             return 0;
         }
         else {
