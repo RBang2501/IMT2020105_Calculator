1b236382a2a0479ba50dceacb9849afb71a09fde
RBang2501 <rakshitbang0005@gmail.com>
2023-11-07 13:54:09 +0530

    Testing Final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,65 @@
 package org.example;
+import java.util.Scanner;
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+    public static double division(int a, int b) {
+        if (b == 0) {
+            System.out.println("Sorry ! Division by Zero Not Possible.");
+            return 0;
+        }
+        else {
+            return (double) a / b;
+        }
+    }
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello World Rakshit Not he");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Rakshit's Calculator !");
+        while(true){
+            System.out.println("Here are the operations. Please Select Your Choice.");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+            int operation = sc.nextInt();
+            if(operation == 5){
+                System.out.println("Thankyou!");
+                break;
+            }
+            System.out.println("Enter the two Numbers: a b");
+            int a = sc.nextInt();
+            sc.nextLine();
+            int b = sc.nextInt();
+            sc.nextLine();
+            if(operation == 1){
+                System.out.println(addition(a, b));
+            }
+            else if (operation == 2){
+                System.out.println(subtraction(a, b));
+            }
+            else if(operation ==3){
+                System.out.println(multiplication(a, b));
+            }
+            else if(operation == 4)
+            {
+                System.out.println(division(a, b));
+            }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
         }
     }
 }
+
