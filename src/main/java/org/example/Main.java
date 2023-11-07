de37cfdd9f979ef3759f1038969a8a99134f2a90
RBang2501 <rakshitbang0005@gmail.com>
2023-11-07 23:04:28 +0530

    Final Modifications

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,10 +15,10 @@ public class Main {
     public static int multiplication(int a, int b) {
         return a * b;
     }
-    public static double division(int a, int b) {
+    public static double division(int a, int b)
+    {
         if (b == 0) {
-            System.out.println("Sorry ! " +
-                    "division  by 0 not possible");
+            System.out.println("Sorry ! division by 0 not possible");
             return 0;
         }
         else {
@@ -28,7 +28,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Calculator!!");
+        System.out.println("Welcome to Rakshit's Calculator!!");
         while(true){
             System.out.println("Here are the operations. Please Select Your Choice.");
             System.out.println("1. Addition");
@@ -38,7 +38,7 @@ public class Main {
             System.out.println("5. Exit");
             int operation = sc.nextInt();
             if(operation == 5){
-                System.out.println("Thankyou!");
+                System.out.println("Thank-you!");
                 break;
             }
             System.out.println("Enter the two Numbers: a b");
@@ -52,14 +52,12 @@ public class Main {
             else if (operation == 2){
                 System.out.println(subtraction(a, b));
             }
-            else if(operation ==3){
+            else if (operation == 3){
                 System.out.println(multiplication(a, b));
             }
-            else if(operation == 4)
-            {
+            else if (operation == 4) {
                 System.out.println(division(a, b));
             }
-
         }
     }
 }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,32 @@
+import org.example.Main;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+    private Main calculator;
+
+    @Before
+    public void setUp() {calculator = new Main();}
+    @Test
+    public void test_add_positive(){
+        int a = 1;
+        int b = 2;
+        int expectedResult = 3;
+        Assert.assertEquals(expectedResult, calculator.addition(a, b));
+    }
+    @Test
+    public void test_add_negative(){
+        int a = -1;
+        int b = -2;
+        int expectedResult = -3;
+        Assert.assertEquals(expectedResult, calculator.addition(a, b));
+    }
+    @Test
+    public void test_sub_positive(){
+        int a = 1;
+        int b = 2;
+        int expectedResult = -1;
+        Assert.assertEquals(expectedResult, calculator.subtraction(a, b));
+    }
+}
